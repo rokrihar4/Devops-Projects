4a9f4f28c2462c457654a8e9117fd4cff9b3a54a
Rok Rihar <132666203+rokrihar4@users.noreply.github.com>
2025-11-28 08:51:45 +0100

    Update setup-myapp.ps1 script

--- setup-myapp.ps1
+++ setup-myapp.ps1
@@ -2,20 +2,16 @@ param(
   [string]$Name = "myapp"
 )
 
-multipass set local.mounts.enable=true
+multipass set local.privileged-mounts=true
 
-# 1. Ustvari VM + mounta kode in frontenda
 multipass launch jammy `
   --name $Name `
   --cloud-init cloud-init/cloud-config.yaml `
   --mount .\app:/opt/myapp `
   --mount .\frontend\dist:/var/www/html/react
 
-# 2. Počakaj, da cloud-init v VM-ju konča
 multipass exec $Name -- bash -lc "cloud-init status --wait"
 
-# 3. Namesti Python pakete iz mountanega folderja
 multipass exec $Name -- sudo -u ubuntu /opt/venvs/myapp/bin/pip install -r /opt/myapp/requirements.txt
 
-# 4. Restartaj app service (zdaj ima vse pakete)
 multipass exec $Name -- sudo systemctl restart app
